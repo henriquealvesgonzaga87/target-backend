c0ea8d7720a4978f57b7cb493b658269c2f3d36d
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2025-01-16 20:10:31 +0000

    refactors the project's config

--- app/api.py
+++ app/api.py
@@ -1,7 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from sqlalchemy.orm import Session
+
+from domain.entities.user_entities import User
+from domain.usecases.users.usecases import create_user
+from infrastructure.adapters import get_db
+from schemas.user_schema import User as UserSchema
 
 router = APIRouter()
 
 @router.get("")
 def get():
     return {"message": "Hello world!"}
+
+
+@router.post("/users/", response_model=User)
+def create_user_view(user: UserSchema, db: Session = Depends(get_db)):
+    return create_user(user.dict(), db)
